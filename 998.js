ab6bc961fa7f40be8eade75ec6e3d9f88920f79b
Takuya-Ampi <ap.takuya@gmail.com>
2020-03-16 23:56:44 +0900

    problem 998 修正

--- 998.js
+++ 998.js
@@ -8,11 +8,11 @@ reader.on("line", line => {
 });
 reader.on("close", () => {
   const newArr = lines[0].split(" ").map(str => parseInt(str, 10))
-  function compareFunc(a, b) {
-    return a - b;
+  const compare = (a, b) => {
+    a - b;
   }
   newArr.sort(compareFunc)
-  let sum = newArr.reduce((a, b) => a + b)
+  const sum = newArr.reduce((a, b) => a + b)
   const check = 4 * newArr[0] + 6
   if (sum === check) {
     console.log('Yes')
