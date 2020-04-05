8bc83ff917382642f19748a29b0a4aad5c2328f8
行比拓也 <imac@iMac.local>
2020-04-05 18:03:32 +0900

    pr 830

---
+++ 830.js
@@ -0,0 +1,11 @@
+const reader = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let lines = [];
+reader.on("line", line => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  console.log(parseInt(lines[0] * 10));
+});
